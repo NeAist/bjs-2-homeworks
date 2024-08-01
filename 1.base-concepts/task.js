4c8b35ac5778483e35e404b954adac52302600bc
Roman <neaist@mail.ru>
2024-08-01 22:41:09 +0300

    Выполнил второе задание, поправил объявление переменных в первом

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,7 +2,7 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let r1, r2;
-  let d = Math.pow(b, 2) - 4 * a * c;
+  const d = Math.pow(b, 2) - 4 * a * c;
   if (d == 0) {
     r1 = -b / (2 * a);
     arr.push(r1);
@@ -15,9 +15,9 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P = percent / 100 / 12;
-  let n = countMonths;
-  let S = amount - contribution;
+  const P = percent / 100 / 12;
+  const n = countMonths;
+  const S = amount - contribution;
   let monthlyPayment;
   let totalMortrage;
   monthlyPayment = S * (P + P / ((1 + P) ** n - 1));
