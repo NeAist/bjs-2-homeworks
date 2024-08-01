fe38752bf0be768932760c977f4754036d194793
Roman <neaist@mail.ru>
2024-08-01 22:48:35 +0300

    Доработка второго задания

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -81,7 +81,7 @@ function averageEvenElementsWorker(...arr) {
 
 function makeWork(arrOfArr, func) {
   let maxWorkerResult = -Infinity;
-  
+
   for (let i = 0; i < arrOfArr.length; i++) {
     const result = func(...arrOfArr[i]);
     if (result > maxWorkerResult) {
@@ -89,5 +89,5 @@ function makeWork(arrOfArr, func) {
     }
   }
 
-  return maxWorkerResult
+  return maxWorkerResult;
 }
